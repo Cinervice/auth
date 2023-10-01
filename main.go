068614641dd796d8a652b8adf4be7528e170e7a0
Ehsan Ahmadi Kian <ehsan_amd72@hotmail.com>
2023-10-01 14:10:03 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	gohandlers "github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/shadowshot-x/micro-product-go/authservice"
+)
+
+func main() {
+	mainRouter := mux.NewRouter()
+
+	authRouter := mainRouter.PathPrefix("/auth").Subrouter()
+	authRouter.HandleFunc("/signup", authservice.SignupHandler).Methods("POST")
+
+	authRouter.HandleFunc("/signin", authservice.SigninHandler).Methods("GET")
+
+	ch := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"http://localhost:3000"}))
+
+	server := &http.Server{
+		Addr:    "127.0.0.1:9090",
+		Handler: ch(mainRouter),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error Booting the Server")
+	}
+}
